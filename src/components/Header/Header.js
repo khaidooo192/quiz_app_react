27c7add721548c6f52d52da4d10718c739ff873d
Khai Do <khaidooo192@gmail.com>
2024-10-08 20:16:52 +0700

    router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,28 +2,33 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from "react-router-dom";
+
 
 const Header = () => {
     return (
         <Navbar bg='light' expand="lg" >
             <Container>
-                <Navbar.Brand href="#home">OK QUIZ</Navbar.Brand>
+                {/* <Navbar.Brand href="">OK QUIZ</Navbar.Brand> */}
+                <NavLink to='/' className='navbar-brand'>OK QUIZ</NavLink>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Users</Nav.Link>
-                        <Nav.Link href="#link">Admin</Nav.Link>
+                        <NavLink to='/' className='nav-link'>Home</NavLink>
+                        <NavLink to='/users' className='nav-link'>Users</NavLink>
+                        <NavLink to='/admins' className='nav-link'>Admin</NavLink>
+
 
                     </Nav>
                     <Nav>
-                        <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Logout
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                        <button className='btn-login'>Login</button>
+                        <button className='btn-signup'>Sign up</button>
 
+                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                            <NavDropdown.Item>Login</NavDropdown.Item>
+                            <NavDropdown.Item>Logout</NavDropdown.Item>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
