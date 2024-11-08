b90b5bf2e4780b719525a322ff0398a9e2e0e753
Khai Do <khaidooo192@gmail.com>
2024-11-08 23:52:20 +0700

    process data question

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,21 +1,42 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
-
+import _ from 'lodash'
 const DetailQuiz = (props) => {
     const params = useParams();
     const quizId = params.id;
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchQuestion();
-    },[quizId]);
+    }, [quizId]);
 
-    const fetchQuestion = async()=>{
+    const fetchQuestion = async () => {
         let res = await getDataQuiz(quizId)
         console.log('check question : ', res);
-        
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (id), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers)
+                    })
+                    return { questionId: key, answers, questionDescription, image }
+                })
+                .value()
+            console.log(data);
+        }
+
     }
-    
+
     return (
         <div className="detail-quiz-container">
             DetailQuiz
