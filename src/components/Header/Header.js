7a02fec2aa3c8752cdd954315ede778d17b81fb5
Khai Do <khaidooo192@gmail.com>
2025-03-03 23:25:58 +0700

    route design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,26 +2,26 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from "react-router-dom";
+
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Quizz Demo</Navbar.Brand>
+        <NavLink to="/" className="navbar-brand" >Quizz Demo</NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">User</Nav.Link>
-            <Nav.Link href="#link">Admin</Nav.Link>
+            <NavLink to="/" className='nav-link'>Home</NavLink>
+            <NavLink to="/users" className='nav-link'>User</NavLink>
+            <NavLink to="/admins" className='nav-link'>Admin</NavLink>
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Log out
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>              
+              <NavDropdown.Item >Login</NavDropdown.Item>
+              <NavDropdown.Item >Log out</NavDropdown.Item>
+              <NavDropdown.Item >Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
