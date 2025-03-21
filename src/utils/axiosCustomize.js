56fd0d3deab9bb39cccd9511bc4c7def24768a37
Khai Do <khaidooo192@gmail.com>
2025-03-21 21:41:04 +0700

    axios custommize fix

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -12,6 +12,8 @@ instance.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 
+
+
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
